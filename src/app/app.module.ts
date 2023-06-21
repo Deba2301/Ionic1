f29d38d19e39d1cada23510486b65c85995b61dd
Deba301 <paridadeashish2301@gmail.com>
2023-06-21 14:24:56 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppStoreModule } from 'src/store/AppStoreModule';
+
+
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  ...AppStoreModule,
+  
+ 
 })
 export class AppModule {}
